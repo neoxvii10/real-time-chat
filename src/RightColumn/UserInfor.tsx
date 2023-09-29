86659c3b75da73e396713924d59c3109151fc6a8
unknown <stkey002@gmail.com>
2023-09-29 15:29:34 +0700

    add edit button and media animation

--- src/RightColumn/UserInfor.tsx
+++ src/RightColumn/UserInfor.tsx
@@ -1,7 +1,8 @@
 import { CSSProperties, useState } from "react";
 import { IoMdClose } from "react-icons/io";
-import { HiOutlinePencil } from "react-icons/hi2";
+import { RiPencilLine } from "react-icons/ri";
 import { MdOutlineCall } from "react-icons/md";
+import { IoMdArrowBack } from "react-icons/io";
 
 import "./UserInfor.css";
 
@@ -19,43 +20,91 @@ type UserInfoProps = {
 };
 
 const RigtColumn: React.FC<UserInfoProps> = ({ userInfoProp }) => {
+  const [editClicked, setEditClick] = useState<boolean>(false);
+  const [mediaClicked, setMediaClick] = useState<string>("unclicked");
+
+  const handleClickOnEditButton = (
+    event: React.MouseEvent<HTMLSpanElement>
+  ) => {
+    event.preventDefault();
+    if (editClicked) {
+      setEditClick(false);
+    } else {
+      setEditClick(true);
+    }
+  };
+
+  const handleClickMedia = (event: React.MouseEvent<HTMLParagraphElement>) => {
+    const content = event.currentTarget.innerHTML;
+    setMediaClick(content);
+    console.log(mediaClicked);
+  };
+
   return (
     <div className="edit-info-container">
       <div className="edit-info-header">
-        <span className="btn-close">
-          <IoMdClose size={24} className="util-icon" />
-        </span>
-        <h5>User info</h5>
-        <span className="btn-edit">
-          <HiOutlinePencil size={24} className="util-icon" />
-        </span>
+        {editClicked ? (
+          <div>
+            <span className="btn-edit" onClick={handleClickOnEditButton}>
+              <IoMdArrowBack size={24} className="util-icon" />
+            </span>
+            <h3>Edit</h3>
+          </div>
+        ) : (
+          <div>
+            <span className="btn-close">
+              <IoMdClose size={24} className="util-icon" />
+            </span>
+            <h3>User info</h3>
+            <span className="btn-edit" onClick={handleClickOnEditButton}>
+              <RiPencilLine size={24} className="util-icon" />
+            </span>
+          </div>
+        )}
       </div>
+      {editClicked ? (
+        <div></div>
+      ) : (
+        <div>
+          <div className="avatar-container">
+            <p className="avatar">{userInfoProp.avatar}</p>
+            <p className="name">{userInfoProp.name}</p>
+          </div>
 
-      <div className="avatar-container">
-        <p className="avatar">{userInfoProp.avatar}</p>
-        <p className="name">{userInfoProp.name}</p>
-      </div>
+          <div className="phone-container">
+            <div className="layout-btn">
+              <MdOutlineCall size={24} className="util-icon" />
+              <p>Phone number</p>
+            </div>
+          </div>
 
-      <div className="phone-container">
-        <div className="layout-btn">
-          <MdOutlineCall size={24} className="util-icon" />
-          <p>Phone number</p>
+          <div className="media-container">
+            <div className="header">
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Media
+              </p>
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Files
+              </p>
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Links
+              </p>
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Music
+              </p>
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Voice
+              </p>
+              <p className="media-topic" onClick={(e) => handleClickMedia(e)}>
+                Group
+              </p>
+            </div>
+            <div className="body">
+              <p>no media files jett</p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="media-container">
-        <div className="header">
-          <p className="media-topic">Media</p>
-          <p className="media-topic">Files</p>
-          <p className="media-topic">Links</p>
-          <p className="media-topic">Music</p>
-          <p className="media-topic">Voice</p>
-          <p className="media-topic">Group</p>
-        </div>
-        <div className="body">
-          <p>no media files jett</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/Users/Users.tsx
+++ src/Users/Users.tsx
@@ -1,21 +1,21 @@
-import { useState, useRef } from 'react';
-import './Users.css';
-import { FiMenu } from 'react-icons/fi'
-import { GoSearch, GoX } from 'react-icons/go' 
-import { BsCloudCheck, BsPerson } from 'react-icons/bs'
-import { LuSettings } from 'react-icons/lu'
-import { WiMoonAltThirdQuarter } from 'react-icons/wi'
-import { TfiArrowLeft } from 'react-icons/tfi'
-import DarkMode from './DarkMode/DarkMode';
+import { useState, useRef } from "react";
+import "./Users.css";
+import { FiMenu } from "react-icons/fi";
+import { GoSearch, GoX } from "react-icons/go";
+import { BsCloudCheck, BsPerson } from "react-icons/bs";
+import { LuSettings } from "react-icons/lu";
+import { WiMoonAltThirdQuarter } from "react-icons/wi";
+import { TfiArrowLeft } from "react-icons/tfi";
+import DarkMode from "./DarkMode/DarkMode";
 
 type userProp = {
-  name: string,
-  id: string,
-  avatar: string,
-  chat: string,
-  time: string,
-  no_id: number,
-}
+  name: string;
+  id: string;
+  avatar: string;
+  chat: string;
+  time: string;
+  no_id: number;
+};
 
 type UsersProps = {
   onUserClick: (selectedUsername: userProp) => void;
@@ -30,152 +30,174 @@ const Users: React.FC<UsersProps> = ({ onUserClick }) => {
       chat: "Đc thế nhờ",
       time: "Sep 14",
       no_id: 1,
-     },
-     {
+    },
+    {
       name: "Phạm Tùng Thủy",
       id: "#@thuypham412",
       avatar: "PT",
       chat: "Đc thế nhờ",
       time: "Sep 13",
       no_id: 2,
-     },
-     {
+    },
+    {
       name: "Đinh Đức Thuận",
       id: "#@thuandinh795",
       avatar: "DT",
       chat: "Đc thế nhờ",
       time: "Sep 7",
       no_id: 3,
-     },
-     {
+    },
+    {
       name: "Nguyễn Trung Hiếu",
       id: "#@hieunguyen318",
       avatar: "NH",
       chat: "Đc thế nhờ",
       time: "Sep 8",
       no_id: 4,
-     },
-     {
+    },
+    {
       name: "Triệu Thanh Tùng",
       id: "#@tungtrieu799",
       avatar: "TT",
       chat: "Đc thế nhờ",
       time: "Sep 12",
       no_id: 5,
-     },
-     {
+    },
+    {
       name: "Trần Tất Việt",
       id: "#@viettt132",
       avatar: "TV",
       chat: "Đc thế nhờ",
       time: "Sep 11",
       no_id: 6,
-     },
+    },
   ];
 
   const [isClick, setIsClick] = useState<boolean>(false);
   const [isMenuRotated, setMenuRotated] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>("");
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   // Handle styles for search bar when clicked and Menu-Back Icon
   const handleOnClick = (event: React.MouseEvent<Element, MouseEvent>) => {
     const target = event.target as HTMLDivElement;
-    if (target.classList.contains('back-icon')) {
+    if (target.classList.contains("back-icon")) {
       setIsClick(false);
-      setMenuRotated(prevState => !prevState);
-    } else if (isClick && inputRef.current?.value === '') {
+      setMenuRotated((prevState) => !prevState);
+    } else if (isClick && inputRef.current?.value === "") {
       setIsClick(false);
-      setMenuRotated(prevState => !prevState);
+      setMenuRotated((prevState) => !prevState);
     } else if (!isClick) {
       setIsClick(true);
-      setMenuRotated(prevState => !prevState);
+      setMenuRotated((prevState) => !prevState);
     }
   };
 
   // Handle input in search bar
   const handleClearInput = () => {
-    setInputValue('');
+    setInputValue("");
   };
-  
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
-  
+
   // handle menu dropdown
   const [isMenuVisible, setMenuVisible] = useState(false);
 
   const handleMenuClick = () => {
     setMenuVisible(!isMenuVisible);
   };
-  
+
   return (
     <div className="users-container">
       <div className="navigation-users-container">
         <span className="menu-icon-container">
-          {isClick ?
-          <TfiArrowLeft size={22}
-          className={`back-icon ${isMenuRotated ? 'rotated-cw' : 'rotated-ccw'}`}
-          onClick={(e) => handleOnClick(e)}
-          />
-          :
-          <FiMenu
-            size={22}
-            className={`menu-icon ${isMenuRotated ? 'rotated-cw' : 'rotated-ccw'}`}
-            onClick={handleMenuClick}
-          />
-          }
-          <div className="menu-container" style={{
-            visibility: isMenuVisible ? 'visible' : 'hidden',
-            opacity: isMenuVisible ? 1 : 0,
-          }}
-          onMouseLeave={() => setMenuVisible(false)}
+          {isClick ? (
+            <TfiArrowLeft
+              size={22}
+              className={`back-icon ${
+                isMenuRotated ? "rotated-cw" : "rotated-ccw"
+              }`}
+              onClick={(e) => handleOnClick(e)}
+            />
+          ) : (
+            <FiMenu
+              size={22}
+              className={`menu-icon ${
+                isMenuRotated ? "rotated-cw" : "rotated-ccw"
+              }`}
+              onClick={handleMenuClick}
+            />
+          )}
+          <div
+            className="menu-container"
+            style={{
+              visibility: isMenuVisible ? "visible" : "hidden",
+              opacity: isMenuVisible ? 1 : 0,
+            }}
+            onMouseLeave={() => setMenuVisible(false)}
           >
             <ul>
               <li>
-                <span className='dropdown-icon'><BsCloudCheck size={22}/></span>
-                <span className='dropdown-label'>My Cloud</span>
+                <span className="dropdown-icon">
+                  <BsCloudCheck size={22} />
+                </span>
+                <span className="dropdown-label">My Cloud</span>
               </li>
               <li>
-                <span className='dropdown-icon'><BsPerson size={22}/></span>
-                <span className='dropdown-label'>Contacts</span>
+                <span className="dropdown-icon">
+                  <BsPerson size={22} />
+                </span>
+                <span className="dropdown-label">Contacts</span>
               </li>
               <li>
-                <span className='dropdown-icon'><WiMoonAltThirdQuarter size={22}/></span>
+                <span className="dropdown-icon">
+                  <WiMoonAltThirdQuarter size={22} />
+                </span>
                 <div className="dropdown-label-container">
-                  <span className='dropdown-label'>Dark Mode</span>
+                  <span className="dropdown-label">Dark Mode</span>
                   <DarkMode />
                 </div>
               </li>
               <li>
-                <span className='dropdown-icon'><LuSettings size={22}/></span>
-                <span className='dropdown-label'>Settings</span>
+                <span className="dropdown-icon">
+                  <LuSettings size={22} />
+                </span>
+                <span className="dropdown-label">Settings</span>
               </li>
             </ul>
           </div>
         </span>
-        <div className={`search-container ${isClick ? 'active' : ''}`}
-        onClick={handleOnClick}
+        <div
+          className={`search-container ${isClick ? "active" : ""}`}
+          onClick={handleOnClick}
         >
-          <div className='search-bar'>
-          <span className="search-icon-container">
-            <GoSearch size={22} className={`search-icon ${isClick ? 'active' : ''}`}/>
-          </span>
-          <form action="">
-            <input
-              ref={inputRef}
-              type="text"
-              placeholder='Search'
-              value={inputValue}
-              onChange={handleInputChange}
-            />
-          </form>
+          <div className="search-bar">
+            <span className="search-icon-container">
+              <GoSearch
+                size={22}
+                className={`search-icon ${isClick ? "active" : ""}`}
+              />
+            </span>
+            <form action="">
+              <input
+                ref={inputRef}
+                type="text"
+                placeholder="Search"
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+            </form>
           </div>
-          
+
           {inputValue && ( // Render the clear icon only if there is input value
             <span className="clear-icon-container" onClick={handleClearInput}>
-              <GoX size={22} className={`clear-icon ${isClick ? 'active' : ''}`} />
+              <GoX
+                size={22}
+                className={`clear-icon ${isClick ? "active" : ""}`}
+              />
             </span>
           )}
         </div>
@@ -183,21 +205,21 @@ const Users: React.FC<UsersProps> = ({ onUserClick }) => {
       <div className="chatlist-container">
         <ul>
           {users.map((user) => (
-            <li tabIndex={user.no_id} key={user.no_id} onClick={() => onUserClick(user)}>
+            <li
+              tabIndex={user.no_id}
+              key={user.no_id}
+              onClick={() => onUserClick(user)}
+            >
               <div className="user">
                 <div className="user-avatar">
-                  <span>
-                    {user.avatar}
-                  </span>
+                  <span>{user.avatar}</span>
                 </div>
                 <div className="user-label-timestamps">
                   <div className="user-labels">
                     <h5>{user.name}</h5>
                     <p>{user.chat}</p>
                   </div>
-                  <span className="latest-timestamps">
-                    {user.time}
-                  </span>
+                  <span className="latest-timestamps">{user.time}</span>
                 </div>
               </div>
             </li>
@@ -206,6 +228,6 @@ const Users: React.FC<UsersProps> = ({ onUserClick }) => {
       </div>
     </div>
   );
-}
+};
 
 export default Users;
