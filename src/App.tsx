d0e1e018c31e62ebb01ef860cf0d695108dcdf53
TranTatViet21020132 <100561242+TranTatViet21020132@users.noreply.github.com>
2023-10-17 16:17:19 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import HomePage from './HomePage/HomePage';
+import Signup from './Authentications/Signup/Signup'
+import Signin from './Authentications/Signin/Signin'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:username" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </Router>
   );
