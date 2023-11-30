83facc6e4b422df6fadd11c431b50cac3ac6693f
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2023-11-30 23:38:02 +0700

    Basic top bar

--- src/Dashboard/Admin.tsx
+++ src/Dashboard/Admin.tsx
@@ -1,6 +1,10 @@
 import Topbar from './scenes/global/Topbar';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Sidebar from './scenes/global/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './scenes/dashboard/Dashboard';
+
 const Admin= () => {
     const {theme, colorMode} = useMode();
 
@@ -11,6 +15,9 @@ const Admin= () => {
                 <div className="admin">
                     <main className="content">
                         <Topbar/>
+                        <Routes>
+                            <Route path="/" element={<Dashboard/>}/>
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
