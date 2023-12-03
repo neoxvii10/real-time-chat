4757c979e0375a515225b9396767acb01b1b7c81
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2023-12-03 20:48:19 +0700

    Bar, Line, Pie and Geography Charts with Nivo Charts

--- src/Dashboard/Admin.tsx
+++ src/Dashboard/Admin.tsx
@@ -11,6 +11,10 @@ import Invoices from './scenes/invoices/Invoices';
 import Form from './scenes/form/Form';
 import Calendar from './scenes/calendar/Calendar';
 import FAQ from './scenes/faq/FAQ';
+import Bar from './scenes/bar/Bar';
+import Pie from './scenes/pie/Pie';
+import Line from './scenes/line/Line';
+import Geography from './scenes/geography/Geography';
 const Admin= () => {
     const {theme, colorMode} = useMode();
 
@@ -28,11 +32,11 @@ const Admin= () => {
                             <Route path="/contacts" element={<Contacts/>}/>
                             <Route path="/invoices" element={<Invoices/>}/>
                             <Route path="/form" element={<Form/>}/>
-                            <Route path="/bar" element={<Dashboard/>}/>
-                            <Route path="/pie" element={<Dashboard/>}/>
-                            <Route path="/line" element={<Dashboard/>}/>
+                            <Route path="/bar" element={<Bar/>}/>
+                            <Route path="/pie" element={<Pie/>}/>
+                            <Route path="/line" element={<Line/>}/>
                             <Route path="/faq" element={<FAQ/>}/>
-                            <Route path="/geography" element={<Dashboard/>}/>
+                            <Route path="/geography" element={<Geography/>}/>
                             <Route path="/calendar" element={<Calendar/>}/>
                         </Routes>
                     </main>
